70fb3c6a713403e32c65043612b52fff5648fffb
rakesh <rakesh@binarynote.com>
2018-05-24 20:20:09 +0530

    Raising 404 error when ID does not exist

--- music/urls.py
+++ music/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),       #blank url    http://127.0.0.1
     path('music/', views.index, name="index"),  # http://127.0.0.1/music
-    path('<int:id>/', views.home, name='detail'),
+    path('<int:id>/', views.detail, name='detail'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.http import Http404
+from django.shortcuts import render
 from .models import Album
 
 def index(request):
@@ -8,9 +9,14 @@ def index(request):
     # for album in all_album:
     #     html = html+"<li><a href='/"+str(album.id)+ "'>"+ album.artist +" </a></li>"
     # html +='</ul>'
-    template = loader.get_template('music/index.html')
-    context = {'all_album':all_album}
-    return HttpResponse(template.render(context,request))
+    # template = loader.get_template('music/index.html')
 
-def home(request,id):
-    return HttpResponse("This is home page"+str(id))
+    # context = {'all_album':all_album}
+    return render(request,'music/index.html',{'all_album':all_album})
+
+def detail(request,id):
+    try:
+        album = Album.objects.get(pk=id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist...Try again")
+    return render(request,'music/detail.html',{'album':album})
